8e01e5dce0c150f87e743f12a85e55f162b001fd
Octavio <j.o.cuevas53@gmail.com>
2024-02-07 22:35:11 -0600

    CAmbios finales para la entrega de los ejercicios

--- app/src/main/java/com/jordicuevas/ejercicio1/sendreceive/ReceiveActivity.kt
+++ app/src/main/java/com/jordicuevas/ejercicio1/sendreceive/ReceiveActivity.kt
@@ -1,12 +1,44 @@
 package com.jordicuevas.ejercicio1.sendreceive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.jordicuevas.ejercicio1.R
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ReceiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receive)
+
+        val nombre = intent.getStringExtra("PERSON_NOMBRE")
+        val pedido = intent.getStringExtra("PERSON_PEDIDO")
+        val picante = intent.getStringExtra("PERSON_PICANTE")
+
+        val formatter = SimpleDateFormat("HH:mm a")
+        val date = Date()
+        val current = formatter.format(date)
+        val duracion = (5..20).random()
+
+        val tvPedido = findViewById<TextView>(R.id.tvPedido)
+        val btRegreso = findViewById<Button>(R.id.btRegreso)
+
+        tvPedido.text = "$nombre, su pedido de $pedido con $picante llegará pronto"
+
+        btRegreso.setOnClickListener{
+            val resultIntent = Intent().apply() {
+                putExtra("EXTRA_TIME", current)
+                putExtra("EXTRA_DURATION", duracion.toString())
+            }
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
+
+
+
+
 }
--- app/src/main/java/com/jordicuevas/ejercicio1/sendreceive/SendActivity.kt
+++ app/src/main/java/com/jordicuevas/ejercicio1/sendreceive/SendActivity.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.jordicuevas.ejercicio1.R
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class SendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,28 +17,28 @@ class SendActivity : AppCompatActivity() {
         setContentView(R.layout.activity_send)
 
         val btSend = findViewById<Button>(R.id.botonPedir)
+        val tvSolicitud = findViewById<TextView>(R.id.tvSolicitud)
         val pedidos = listOf("Torta ahogada", "Torta cubana", "Torta rusa", "Tacos de birria")
-        val formatter = SimpleDateFormat("yyyy-MM-dd")
-        val date = Date()
-        val current = formatter.format(date)
+        val picante = listOf("Jalapeño", "Chipotle", "Habanero")
+
 
         btSend.setOnClickListener {
-            val sendInfoPedido = Intent(this, ReceiveActivity::class.java).apply{
-                //putExtra("PERSON_CURP", "RAGM770826HZSMZR54")
+            val sendInfoPedido = Intent(this, ReceiveActivity::class.java).apply {
                 putExtra("PERSON_NOMBRE", "Octavio")
                 putExtra("PERSON_PEDIDO", pedidos.random())
-                putExtra("PERSON_FECHA", "Sánchez")
+                putExtra("PERSON_PICANTE", picante.random())
             }
 
             //startActivity(sendInfoPersona)
-            resultRegister.launch(sendInfoPersona)
+            resultRegister.launch(sendInfoPedido)
 
+        }
     }
-
     private val resultRegister = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
         if (result.resultCode == RESULT_OK){
             val tvSolicitud = findViewById<TextView>(R.id.tvSolicitud)
-            tvSolicitud.text = "Tu pedido de ${result.data?.getStringExtra(("EXTRA_HOUR"))} está en camino"
+            tvSolicitud.text = "Tu pedido fue registrado a las ${result.data?.getStringExtra(("EXTRA_TIME"))}, " +
+                    "llegará en ${result.data?.getStringExtra(("EXTRA_DURATION"))} minutos"
             Toast.makeText(this, "Gracias por su pedido", Toast.LENGTH_LONG).show()
         }else{
             Toast.makeText(this, "CANCELLED", Toast.LENGTH_SHORT).show()
