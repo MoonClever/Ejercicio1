1741fca24f26d7a7cbf0957ea6f8ecde83f04f36
Octavio <j.o.cuevas53@gmail.com>
2024-02-15 04:08:29 -0600

    Cambios iniciales para realizar los ejercicios de layouts

--- app/src/main/java/com/jordicuevas/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/jordicuevas/ejercicio1/MainActivity.kt
@@ -4,32 +4,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.jordicuevas.ejercicio1.ejercicios.CicloVida
-import com.jordicuevas.ejercicio1.ejercicios.UrlActivity
-import com.jordicuevas.ejercicio1.sendreceive.SendActivity
+import com.jordicuevas.ejercicio1.ejercicios2.ConstraintLayoutActivity
+import com.jordicuevas.ejercicio1.ejercicios2.FrameLayoutActivity
+import com.jordicuevas.ejercicio1.ejercicios2.LinearLayoutActivity
+import com.jordicuevas.ejercicio1.ejercicios2.RecyclerViewActivity
+import com.jordicuevas.ejercicio1.ejercicios2.RelativeLayoutActivity
+import com.jordicuevas.ejercicio1.ejercicios2.UIControllerActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    val botonCiclo = findViewById<Button>(R.id.botonCiclo)
-    val botonURL = findViewById<Button>(R.id.botonURL)
-    val bontonER = findViewById<Button>(R.id.botonER)
+    val btFrame = findViewById<Button>(R.id.btFrameLayout)
+    val btLinear = findViewById<Button>(R.id.btLinearLayout)
+    val btRelative = findViewById<Button>(R.id.btRelativeLayout)
+    val btConstraint = findViewById<Button>(R.id.btConstraintLayout)
+    val btUIControls = findViewById<Button>(R.id.btUIControls)
+    val btRecyclerView = findViewById<Button>(R.id.btRecyclerview)
 
-    botonCiclo.setOnClickListener{
-        val sendIntent = Intent(this, CicloVida::class.java)
+    btFrame.setOnClickListener{
+        val sendIntent = Intent(this, FrameLayoutActivity::class.java)
         startActivity(sendIntent)
     }
 
-    botonURL.setOnClickListener{
-        val sendIntent = Intent(this, UrlActivity::class.java)
+    btLinear.setOnClickListener{
+        val sendIntent = Intent(this, LinearLayoutActivity::class.java)
         startActivity(sendIntent)
     }
 
-    bontonER.setOnClickListener{
-        val sendIntent = Intent(this, SendActivity::class.java)
+    btRelative.setOnClickListener{
+        val sendIntent = Intent(this, RelativeLayoutActivity::class.java)
         startActivity(sendIntent)
     }
+
+    btConstraint.setOnClickListener{
+        val sendIntent = Intent(this, ConstraintLayoutActivity::class.java)
+        startActivity(sendIntent)
+    }
+
+    btUIControls.setOnClickListener{
+        val sendIntent = Intent(this, UIControllerActivity::class.java)
+        startActivity(sendIntent)
+    }
+
+    btRecyclerView.setOnClickListener{
+        val sendIntent = Intent(this, RecyclerViewActivity::class.java)
+        startActivity(sendIntent)
+    }
+
     }
 }
---
+++ app/src/main/java/com/jordicuevas/ejercicio1/ejercicios2/RelativeLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.jordicuevas.ejercicio1.ejercicios2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jordicuevas.ejercicio1.R
+
+class RelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
